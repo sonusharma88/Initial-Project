ef1bedbdf53f1c371a20430cc7f20f6c8b4f7337
Sonu Sharma <sonusharmacse88@gmail.com>
2021-07-04 20:37:52 +0530

    first project

---
+++ src/Aradhya.java
@@ -0,0 +1,10 @@
+public class Aradhya {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Ram is a good boy");
+        System.out.println("This is my new project");
+        System.out.println("This is a new laptop");
+        System.out.println("This is a new beg");
+
+    }
+}
